4df0176a771a7ceed910019a74676ce971073562
Tomaž kunst <cofek0@gmail.com>
2022-10-26 20:40:38 +0200

    reorder agenda items

--- run_nightly_parser_flow.sh
+++ run_nightly_parser_flow.sh
@@ -9,12 +9,14 @@ echo "start adding absent votes"
 python manage.py create_absent_ballots_for_voters
 echo "start setting votes results"
 python manage.py set_votes_result --majority relative_normal
-echo "start setting legislation results"
+#echo "start setting legislation results"
 # python manage.py set_legislation_results
 # echo "start setting motion tags"
 # python manage.py set_motion_tags
 # echo "start pairing votes with speeches"
 # python manage.py pair_votes_and_speeches
+echo "start reorder speeches"
+python manage.py reset_order_with_agenda_items
 echo "lematize speeches"
 python manage.py lemmatize_speeches
 echo "set tfidf"
