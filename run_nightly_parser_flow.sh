7f71d4618fe90b3d7dba331054d1ae6d3a988a1c
Tomaž kunst <cofek0@gmail.com>
2023-05-05 15:12:52 +0200

    remove unnecessary files

--- run_nightly_parser_flow.sh
+++ run_nightly_parser_flow.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
-echo "parse speeches, votes, sessions"
-scrapy crawl votes
-scrapy crawl questions
-scrapy crawl speeches
-scrapy crawl comitee
-cd /app
-echo "start adding absent votes"
-python manage.py create_absent_ballots_for_voters
-echo "start setting votes results"
-python manage.py set_votes_result --majority relative_normal
-#echo "start setting legislation results"
-# python manage.py set_legislation_results
-# echo "start setting motion tags"
-# python manage.py set_motion_tags
-# echo "start pairing votes with speeches"
-# python manage.py pair_votes_and_speeches
-echo "start reorder speeches"
-python manage.py reset_order_with_agenda_items
 echo "lematize speeches"
 python manage.py lemmatize_speeches
 echo "set tfidf"
@@ -29,5 +11,9 @@ echo "update speeches to solr"
 python manage.py upload_speeches_to_solr
 echo "update votes to solr"
 python manage.py upload_votes_to_solr
+
+echo "update agenda-items to solr"
+python manage.py upload_agenda_items_to_solr
+
 echo "send notifications"
 python manage.py send_daily_notifications
